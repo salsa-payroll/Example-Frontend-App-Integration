42b1b6a2e1fd6ee4759fd5fc628cf46530c91691
Scott Hogan <scott@salsa.dev>
2023-06-01 15:29:33 -0700

    adjust spacing for content region extract learning text as own component, remove from payroll page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,17 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 function Header() {
-  return (      
-  <div className='header'>
-    <img className='logo' src='/logo.png' width="50" height="50" alt="logo"/>
-    <h1>Union Mate</h1>
-    <div className='profile'>
-      <FontAwesomeIcon color='white' icon={faUser} className='profile-icon'/>
-      <h4>Profile</h4>
+  return (
+    <div className="header">
+      <img className="logo" src="/logo.png" alt="logo" />
+      <h1>Union Mate</h1>
+      <div className="profile">
+        <FontAwesomeIcon color="white" icon={faUser} className="profile-icon" />
+        <h4>Profile</h4>
+      </div>
     </div>
-  </div>)
+  );
 }
 
 export default Header;
